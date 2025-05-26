a20befd8290f287aeb0639e5c11fceeb2932a292
Bongumusa Joseph Hlongwane <94066555+Joemusa@users.noreply.github.com>
2025-05-26 12:09:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 from sklearn.datasets import load_iris
 iris_dataset = load_iris()
-df = pd.DataFrame(data=bunch.data, columns=bunch.feature_names)
+df = pd.DataFrame(data=Bunch.data, columns=Bunch.feature_names)
 st.title('🎈 Machine Learning App')
 
 st.write('This is app builds a machine learning model!')
-df['target'] = bunch.target
+df['target'] = Bunch.target
 
 print(df.head())
 iris_dataset['data'].shape
