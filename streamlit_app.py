5fa1eb37e95f14bc72dbdd9ae59d4c5b2cb146b2
Bongumusa Joseph Hlongwane <94066555+Joemusa@users.noreply.github.com>
2025-05-26 13:03:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import plotly.express as px
 import streamlit as st
 import pandas as pd
 from sklearn.datasets import load_iris
-iris_dataset = load_iris()
+# iris_dataset = load_iris()
 st.title('🎈 Machine Learning Ap')
 
 
