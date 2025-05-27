d25a8bc0fbb82bc383e3fab254dd2bdb4d5c2cf2
Bongumusa Joseph Hlongwane <94066555+Joemusa@users.noreply.github.com>
2025-05-27 23:09:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ with st.expander('Data'):
   y = df1['Species']
   y
   
-with st.expander(**'Statistics'**):
+with st.expander('**Statistics**'):
   st.write('Number of columns and rows')
   show = df1.show
   show
