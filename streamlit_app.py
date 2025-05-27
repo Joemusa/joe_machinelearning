8676eba79d048d0e0af91b419c4f574d867a9ecf
Bongumusa Joseph Hlongwane <94066555+Joemusa@users.noreply.github.com>
2025-05-27 22:48:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,26 +8,9 @@ st.title('🎈 Iris Machine Learning App')
 st.write('This is builds a machine learning model!')
 with st.expander('Data'):
   st.write('**Raw Data**')
-  #csv_url = ('https://raw.githubusercontent.com/Joemusa/joe_machinelearning/refs/heads/master/Iris.csv')
-  #df = pd.read_csv(csv_url)
-  df = pd.DataFrame(data=iris_dataset.data, columns=iris_dataset.feature_names)
-  df['target'] = iris_dataset.target
-  df
-  
-# Extract features (X) and target (y)
-  st.write('**Features**')
-  X = df.drop(columns='target')  # Features (all columns except 'target')
-  X
-  st.write('**Target**')
-  y = df['target']               # Target (labels)
-  y
-
-st.title('Scatter Charts')
-with st.expander('Petal length (cm) vs Petal width (cm)'):
-  st.scatter_chart(data = df, x = 'petal length (cm)', y = 'petal width (cm)', color = 'target' )
-
-with st.expander('Sepal length (cm) vs Sepal width (cm)'):
-  st.scatter_chart(data = df, x = 'sepal length (cm)', y = 'sepal width (cm)', color = 'target' )
+  csv_url = ('https://raw.githubusercontent.com/Joemusa/joe_machinelearning/refs/heads/master/Iris.csv')
+  df = pd.read_csv(csv_url)
+ 
 
 
 
