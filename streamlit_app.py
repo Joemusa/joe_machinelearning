babdd2ad186accc3f09ca5b96ca0c7b377b943cf
Bongumusa Joseph Hlongwane <94066555+Joemusa@users.noreply.github.com>
2025-05-28 08:36:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,7 +90,11 @@ if st.sidebar.button("🔮 Predict Species"):
     st.write(f"Model Accuracy: **{acc_score}%**")
                        
 
-  
+  with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
 
 
 
